12e1ce77f99a0739ed6df6182e89645118180427
PrenitaVulpo <guilhermepdl97@protonmail.com>
2024-03-20 22:41:16 -0300

    last commit for projeto 3

--- projeto 3/script/Token.s.sol
+++ projeto 3/script/Token.s.sol
@@ -1,12 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script, console2} from "forge-std/Script.sol";
+import {Token} from "../src/Token.sol";
 
-contract CounterScript is Script {
+contract TokenScript is Script {
+    Token token;
+
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast(
+            0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+        );
+
+        // deploy do contrato
+        token = new Token();
+
+        console2.log("ADDRESS: ", address(token));
+        // 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+        vm.stopBroadcast();
     }
 }
